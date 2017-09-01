1adecdb6b4354550d97ce8f532c7262e40df5809
Przemysław Walkowski <przemyslaw.walkowski@bccgroup.com>
2017-09-01 14:14:36 +0200

    Wersja 1.0

--- RossaryApp/Program.cs
+++ RossaryApp/Program.cs
@@ -1,139 +1,173 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace RossaryApp
 {
     class Program
     {
-
         private const int h = 50;
 
         private static int[,] rosaryTab = new int[h, 40];
+        private static List<KeyValuePair<int, int>> rosaryXY;
+
+        public Program()
+        {
+            SetRosaryTab();
+        }
 
         static void Main(string[] args)
         {
-
-           
-
             SetRosaryTab();
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
             ConsoleKey key = ConsoleKey.Enter;
 
-            while (key != ConsoleKey.Escape)
-            {
+            var prayIndex = -2;
 
-              
+              while (key != ConsoleKey.Escape && prayIndex!=58)
+            {
                 key = Console.ReadKey(true).Key;
                 Console.Clear();
-                DrawRosary();
+                DrawCross();
+                prayIndex++;
+
+                KeyValuePair<int, int> xy;
+
+                if (prayIndex >= 0)
+                {
+                    xy = rosaryXY[prayIndex];
+                }
+
+                rosaryTab[xy.Key, xy.Value] = 1;
+
+                DrawRosary(rosaryTab);
             }
 
-            
+            stopWatch.Stop();
+
+            Console.WriteLine($"Koniec w {GetStopWatchString(stopWatch.Elapsed)}");
+            Console.ReadKey();
+
+        }
+
+        private static void DrawCross()
+        {
+            var cross = new List<KeyValuePair<int, int>>
+            {
+                new KeyValuePair<int, int>(h-4, 18),
+                new KeyValuePair<int, int>(h-4, 17),
+                new KeyValuePair<int, int>(h-4, 14),
+                new KeyValuePair<int, int>(h-4, 15),
+                new KeyValuePair<int, int>(h-1, 16),
+                new KeyValuePair<int, int>(h-2, 16),
+                new KeyValuePair<int, int>(h-3, 16),
+                new KeyValuePair<int, int>(h-4, 16),
+                new KeyValuePair<int, int>(h-5, 16),
+            };
+
+            foreach (var item in cross)
+            {
+                rosaryTab[item.Key, item.Value] = 1;
+            }
         }
 
         private static void SetRosaryTab()
         {
-            //Prawa strona
+             rosaryXY = new List<KeyValuePair<int, int>>
+            {
+                new KeyValuePair<int, int>(h - 15, 18), //5.10
+                new KeyValuePair<int, int>(h - 16, 19), //5.9
+                new KeyValuePair<int, int>(h - 17, 20), //5.8
+                new KeyValuePair<int, int>(h - 18, 21), //5.7
+                new KeyValuePair<int, int>(h - 19, 22), //5.6
+                new KeyValuePair<int, int>(h - 20, 23), //5.5
+                new KeyValuePair<int, int>(h - 21, 24), //5.4
+                new KeyValuePair<int, int>(h - 22, 25), //5.3
+                new KeyValuePair<int, int>(h - 23, 26), //5.2
+                new KeyValuePair<int, int>(h - 24, 27), //5.1
 
-            rosaryTab[h - 44, 18] = 1;
-            rosaryTab[h - 44, 21] = 1;
-            rosaryTab[h - 43, 24] = 1;
-            rosaryTab[h - 42, 26] = 1;
-            rosaryTab[h - 41, 28] = 1;
+                new KeyValuePair<int, int>(h - 26, 28), //5.0
 
-            rosaryTab[h - 39, 29] = 1;
+                new KeyValuePair<int, int>(h - 28, 29), //4.10
+                new KeyValuePair<int, int>(h - 29, 29), //4.9
+                new KeyValuePair<int, int>(h - 30, 29), //4.8
+                new KeyValuePair<int, int>(h - 31, 29), //4.7
+                new KeyValuePair<int, int>(h - 32, 29), //4.6
+                new KeyValuePair<int, int>(h - 33, 29), //4.5
+                new KeyValuePair<int, int>(h - 34, 29), //4.4
+                new KeyValuePair<int, int>(h - 35, 29), //4.3
+                new KeyValuePair<int, int>(h - 36, 29), //4.2
+                new KeyValuePair<int, int>(h - 37, 29), //4.1
 
-            rosaryTab[h - 37, 29] = 1;
-            rosaryTab[h - 36, 29] = 1;
-            rosaryTab[h - 35, 29] = 1;
-            rosaryTab[h - 34, 29] = 1;
-            rosaryTab[h - 33, 29] = 1;
-            rosaryTab[h - 32, 29] = 1;
-            rosaryTab[h - 31, 29] = 1;
-            rosaryTab[h - 30, 29] = 1;
-            rosaryTab[h - 29, 29] = 1;
-            rosaryTab[h - 28, 29] = 1;
+                new KeyValuePair<int, int>(h - 39, 29), //4.0
+                
+                new KeyValuePair<int, int>(h - 41, 28), //3.10
+                new KeyValuePair<int, int>(h - 42, 26), //3.9
+                new KeyValuePair<int, int>(h - 43, 24), //3.8
+                new KeyValuePair<int, int>(h - 44, 21), //3.7
+                new KeyValuePair<int, int>(h - 44, 18), //3.6
+                new KeyValuePair<int, int>(h - 44, 14), //3.5
+                new KeyValuePair<int, int>(h - 44, 11), //3.4
+                new KeyValuePair<int, int>(h - 43, 7),  //3.3
+                new KeyValuePair<int, int>(h - 42, 5),  //3.2
+                new KeyValuePair<int, int>(h - 41, 3),  //3.1
 
-            rosaryTab[h - 26, 28] = 1;
+                new KeyValuePair<int, int>(h - 39, 2), //3.0
 
-            rosaryTab[h - 24, 27] = 1;
-            rosaryTab[h - 23, 26] = 1;
-            rosaryTab[h - 22, 25] = 1;
-            rosaryTab[h - 21, 24] = 1;
-            rosaryTab[h - 20, 23] = 1;
-            rosaryTab[h - 19, 22] = 1;
-            rosaryTab[h - 18, 21] = 1;
-            rosaryTab[h - 17, 20] = 1;
-            rosaryTab[h - 16, 19] = 1;
-            rosaryTab[h - 15, 18] = 1;
+                new KeyValuePair<int, int>(h - 37, 2), //2.10
+                new KeyValuePair<int, int>(h - 36, 2), //2.9
+                new KeyValuePair<int, int>(h - 35, 2), //2.8
+                new KeyValuePair<int, int>(h - 34, 2), //2.7
+                new KeyValuePair<int, int>(h - 33, 2), //2.6
+                new KeyValuePair<int, int>(h - 32, 2), //2.5
+                new KeyValuePair<int, int>(h - 31, 2), //2.4
+                new KeyValuePair<int, int>(h - 30, 2), //2.3
+                new KeyValuePair<int, int>(h - 29, 2), //2.2
+                new KeyValuePair<int, int>(h - 28, 2), //2.1
 
-            //Lewa strona
+                new KeyValuePair<int, int>(h - 26, 3),  //2.0
 
+                new KeyValuePair<int, int>(h - 24, 4),  //1.10
+                new KeyValuePair<int, int>(h - 23, 5),  //1.9
+                new KeyValuePair<int, int>(h - 22, 6),  //1.8
+                new KeyValuePair<int, int>(h - 21, 7),  //1.7
+                new KeyValuePair<int, int>(h - 20, 8),  //1.6
+                new KeyValuePair<int, int>(h - 19, 9),  //1.5
+                new KeyValuePair<int, int>(h - 18, 10), //1.4
+                new KeyValuePair<int, int>(h - 17, 11), //1.3
+                new KeyValuePair<int, int>(h - 16, 12), //1.2
+                new KeyValuePair<int, int>(h - 15, 13), //1.1
 
-            rosaryTab[h - 44, 14] = 1;
-            rosaryTab[h - 44, 11] = 1;
-            rosaryTab[h - 43, 7] = 1;
-            rosaryTab[h - 42, 5] = 1;
-            rosaryTab[h - 41, 3] = 1;
+                new KeyValuePair<int, int>(h - 13, 16), //1.0
 
-            rosaryTab[h - 39, 2] = 1;
+                new KeyValuePair<int, int>(h - 11, 16), 
+                new KeyValuePair<int, int>(h - 10, 16), 
+                new KeyValuePair<int, int>(h - 9, 16), 
 
-            rosaryTab[h - 37, 2] = 1;
-            rosaryTab[h - 36, 2] = 1;
-            rosaryTab[h - 35, 2] = 1;
-            rosaryTab[h - 34, 2] = 1;
-            rosaryTab[h - 33, 2] = 1;
-            rosaryTab[h - 32, 2] = 1;
-            rosaryTab[h - 31, 2] = 1;
-            rosaryTab[h - 30, 2] = 1;
-            rosaryTab[h - 29, 2] = 1;
-            rosaryTab[h - 28, 2] = 1;
-
-            rosaryTab[h - 26, 3] = 1;
-
-            rosaryTab[h - 24, 4] = 1;
-            rosaryTab[h - 23, 5] = 1;
-            rosaryTab[h - 22, 6] = 1;
-            rosaryTab[h - 21, 7] = 1;
-            rosaryTab[h - 20, 8] = 1;
-            rosaryTab[h - 19, 9] = 1;
-            rosaryTab[h - 18, 10] = 1;
-            rosaryTab[h - 17, 11] = 1;
-            rosaryTab[h - 16, 12] = 1;
-            rosaryTab[h - 15, 13] = 1;
-            
+                new KeyValuePair<int, int>(h - 7, 16) 
+            };
 
 
-            rosaryTab[h - 13, 16] = 1;
-
-            rosaryTab[h - 11, 16] = 1;
-            rosaryTab[h - 10, 16] = 1;
-            rosaryTab[h - 9, 16] = 1;
-
-            rosaryTab[h - 7, 16] = 1;
-
-            rosaryTab[h - 5, 16] = 1;
-            rosaryTab[h - 4, 16] = 1;
-            rosaryTab[h - 3, 16] = 1;
-            rosaryTab[h - 2, 16] = 1;
-            rosaryTab[h - 1, 16] = 1;
-
-            rosaryTab[h - 4, 15] = 1;
-            rosaryTab[h - 4, 14] = 1;
-
-            rosaryTab[h - 4, 17] = 1;
-            rosaryTab[h-4, 18] = 1;
+            rosaryXY.Reverse();
         }
 
-        private static void DrawRosary()
+        public static string GetStopWatchString(TimeSpan ts)
+        {
+            return $"{ts.Hours:00}:{ts.Minutes:00}:{ts.Seconds:00}:{(ts.Milliseconds / 10):00}";
+        }
+
+        private static void DrawRosary(int[,] rosary)
         {
             for (int i = 0; i < h; i++)
             {
                 for (int j = 0; j < 40; j++)
                 {
-                    if (rosaryTab[i, j] == 1)
+                    if (rosary[i, j] == 1)
                     {
-                        Console.Write("*");
+                        Console.Write("O");
                     }
                     else
                     {
@@ -142,7 +176,7 @@ namespace RossaryApp
 
                 }
                 Console.WriteLine("");
-                }
+            }
         }
     }
 }
