a6a575c5234f086e5ff0a6220e246acf435618ee
Przemek <przemyslaw.walkowski@bccgroup.com>
2017-09-12 15:50:32 +0200

    Dodanie koronki i tłumaczeń

---
+++ RossaryApp/Consts.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RossaryApp
+{
+    public static class Consts
+    {
+        public const int h = 50;
+    }
+}
--- RossaryApp/Models/RosaryPrayModel.cs
+++ RossaryApp/Models/RosaryPrayModel.cs
@@ -4,10 +4,15 @@ using System.Text;
 
 namespace RossaryApp.Models
 {
-    public class RosaryPrayModel
+    public class RosaryPrayModel 
     {
         public KeyValuePair<int, int> Coordinate { get; private set; }
-        public string PrayText { get; private set}
+        public string PrayText { get; private set; }
 
+        public RosaryPrayModel(KeyValuePair<int, int> coor, string prayText)
+        {
+            this.Coordinate = coor;
+            this.PrayText = prayText;
+        }
     }
 }
